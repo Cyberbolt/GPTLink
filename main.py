e6b649ea302941a995cb63a0b42648f934335a4f
Cyberbolt <735245473@qq.com>
2023-03-19 13:22:41 +0000

    fastapi init

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="gptlink_gui/build/static"), name="static")
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+with open('gptlink_gui/build/index.html') as f:
+    home_page = f.read()
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def read_root():
-    return {"Hello": "World"}
+    return home_page
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.exception_handler(StarletteHTTPException)
+async def my_custom_exception_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        '''
+            Redirect all 404 pages to index.html
+        '''
+        return HTMLResponse(home_page.text)
