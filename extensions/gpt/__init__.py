d8609f766d4caa9c0fdaa04a2ccd3d4d7615d4ab
Cyberbolt <735245473@qq.com>
2023-03-23 05:29:52 +0000

    GPT backend initialization.

---
+++ extensions/gpt/__init__.py
@@ -0,0 +1,39 @@
+import openai
+
+
+CONVERSAIION = {}
+
+
+def get_history(username: str) -> list:
+    gpt = CONVERSAIION.get(username)
+    if not gpt:
+        return False
+    else:
+        return gpt.messages
+    
+
+def chat(username: str, version: str = 'gpt-3.5-turbo', text: str = '') -> str:
+    if not CONVERSAIION.get(username):
+        CONVERSAIION[username] = GPT(version=version)
+    return CONVERSAIION[username].reply(text)
+
+
+def delete_history(username: str):
+    if CONVERSAIION.get(username):
+        del CONVERSAIION[username]
+
+
+class GPT:
+    def __init__(self, version: str = 'gpt-3.5-turbo'):
+        self.version = version
+        self.messages = []
+
+    def reply(self, text: str):
+        self.messages.append({'role': 'user', 'content': text})
+        response = openai.ChatCompletion.create(
+            model=self.version,
+            messages=self.messages
+        )
+        result = response['choices'][0].message['content']
+        self.messages.append({'role': 'assistant', 'content': result})
+        return result
--- routers/front.py
+++ routers/front.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+from extensions import gpt
+
 
 class Question(BaseModel):
     content: str
@@ -9,6 +11,26 @@ class Question(BaseModel):
 front = APIRouter()
 
 
-front.post('/api/chat')
+front.get('/api/conversation/chat')
+def get_history():
+    history = gpt.get_history(username='user')
+    if not history:
+        return {
+            'content': []
+        }
+    else:
+        return {
+            'content': history
+        }
+
+
+front.patch('/api/conversation/chat')
 def chat(item: Question):
-    return {}
+    reply = gpt.chat(username='user', version='gpt-3.5-turbo', text=item.content)
+    return {'content': reply}
+
+
+front.delete('/api/conversation/chat')
+def delete_history():
+    gpt.delete_history()
+    return True
