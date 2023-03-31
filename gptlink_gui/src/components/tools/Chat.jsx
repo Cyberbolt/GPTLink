5223b1f2b818e5d430724807f960b835857911fe
Cyberbolt <735245473@qq.com>
2023-03-31 05:03:55 +0000

    Automatic code highlighting

--- gptlink_gui/src/components/tools/Chat.jsx
+++ gptlink_gui/src/components/tools/Chat.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react'
 import { Card } from 'antd'
 import { UserOutlined, GlobalOutlined } from '@ant-design/icons'
 import ReactMarkdown from 'react-markdown'
+import remarkHighlight from 'remark-highlight.js'
+import 'highlight.js/styles/vs2015.css'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 import { URL_WS } from '../config/config'
 
@@ -36,7 +40,28 @@ export function ChatBox({ key, question, oldAnswer }) {
             </Card>
             <Card size="small" style={{ marginBottom: '15px' }}>
                 <GlobalOutlined style={{ marginRight: '6px', display:'inline-block' }} />
-                <ReactMarkdown style={{ display:'inline-block' }}>{answer}</ReactMarkdown>
+                <ReactMarkdown 
+                    plugins={[remarkHighlight]} 
+                    children={answer} 
+                    components={{
+                        code({node, inline, className, children, ...props}) {
+                          const match = /language-(\w+)/.exec(className || '')
+                          return !inline && match ? (
+                            <SyntaxHighlighter
+                              style={atomOneDark}
+                              language={match[1]}
+                              PreTag="div"
+                              children={String(children).replace(/\n$/, '')}
+                              {...props}
+                            />
+                          ) : (
+                            <code className={className} {...props}>
+                              {children}
+                            </code>
+                          )
+                        }
+                      }}
+                />
             </Card>
         </>
     )
